bc499a3851c0ece61fe5c02e4d27a254fb75c9e6
Tatianaa88 <tatiana.v.ageeva@gmail.com>
2020-09-27 19:47:01 +0200

    Task 3 - start.

---
+++ Lesson_2/Lesson_2_task_3.py
@@ -0,0 +1,10 @@
+# 3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+
+# list solution
+
+months = ['January', 'February', 'March', 'April',
+          'May', 'June', 'July', 'August',
+          'September', 'October', 'November', 'December']
+
+print(months)
