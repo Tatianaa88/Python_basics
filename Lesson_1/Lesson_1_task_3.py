2b5efc607bc165479af2374cdfc0e44c3b8dd45a
Tatianaa88 <tatiana.v.ageeva@gmail.com>
2020-09-29 09:23:54 +0200

    Task 1 - start.

--- Lesson_1/Lesson_1_task_3.py
+++
@@ -1,7 +0,0 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-usr_num = input('Please enter a random number: ')
-
-print(f'The sum of {usr_num} + {usr_num + usr_num} + {usr_num + usr_num + usr_num} = '
-      f'{int(usr_num) + int(usr_num + usr_num) + int(usr_num + usr_num + usr_num)}')
