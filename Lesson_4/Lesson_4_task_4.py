16e9046023eebd943a74a15cceb80114f4219c5f
Tatianaa88 <tatiana.v.ageeva@gmail.com>
2020-10-06 10:06:36 +0200

    Tasks 4 complete.

--- Lesson_4/Lesson_4_task_4.py
+++ Lesson_4/Lesson_4_task_4.py
@@ -4,8 +4,7 @@
 # Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 # Результат: [23, 1, 3, 10, 4, 11]
 
-empty_list = []
 my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-empty_list = [i for i in my_list for j in my_list]
+new_list = [i for i in my_list if my_list.count(i) == 1]
 
-print(empty_list)
+print(new_list)
