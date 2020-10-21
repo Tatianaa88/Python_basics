0a6c7f63f5fe8f8b61ca2afb09a0c2b3cff06dde
Tatianaa88 <tatiana.v.ageeva@gmail.com>
2020-10-21 20:36:17 +0200

    Loto - done - 1st version.

--- Lesson_8/Lesson_8_Loto.py
+++ Lesson_8/Lesson_8_Loto.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 class LotoCard:
     def __init__(self, player):
@@ -6,30 +7,30 @@ class LotoCard:
         self.card = [['  ','  ','  ','  ','  ','  ','  ','  ','  '],
                      ['  ','  ','  ','  ','  ','  ','  ','  ','  '],
                      ['  ','  ','  ','  ','  ','  ','  ','  ','  ']]
-        self.my_list = []
+        self.number_list = []
 
     def create_number(self):
         num_counter = 1
         while num_counter < 16:
             number = random.randint(1, 90)
-            if number in self.my_list:
+            if number in self.number_list:
                 continue
-            self.my_list.append(number)
+            self.number_list.append(number)
             for i in range(9):
                 similar_num_counter = 0
-                for item in self.my_list:
+                for item in self.number_list:
                     upper_boarder = (i + 1) * 10
                     if i == 8:
                         upper_boarder = 91
                     if i * 10 <= item < upper_boarder:
                         similar_num_counter += 1
                 if similar_num_counter > 3:
-                    self.my_list.pop()
+                    self.number_list.pop()
             num_counter += 1
-        self.my_list.sort()
+        self.number_list.sort()
 
     def fillout_card(self):
-        for el in self.my_list:
+        for el in self.number_list:
             i = 0
             while i < 9:
                 upper_boarder = (i + 1) * 10
@@ -65,29 +66,102 @@ class LotoCard:
                             continue
                 i += 1
 
-class Game(LotoCard):
-    def start(self):
-        self.create_number()
-        self.fillout_card()
-        self.draw_card()
-
     def draw_card(self):
         print('-'* 9 + self.player + '-'* 10)
         for i in self.card:
             for index, j in enumerate(i):
-                if j != '  ' and int(j) < 10:
+                if j != '  ' and j != '--' and int(j) < 10:
                     j = ' ' + str(j)
                 print(j, end=' ')
                 if index % 8 == 0 and index != 0:
                     print('\n'.strip())
         print('-' * 27)
 
-card1 = Game('-Player-')
-card2 = Game('Computer')
-# card1.fillout_card()
-# card1.draw_card()
-# card2.fillout_card()
-# card2.draw_card()
 
-card1.start()
-card2.start()
+class Game:
+    def __init__(self, card1, card2):
+        self.card1 = card1
+        self.card2 = card2
+        self.bag_num = []
+
+    def start(self):
+        card1.create_number()
+        card1.fillout_card()
+        card2.create_number()
+        card2.fillout_card()
+        self.start_game()
+
+
+    def start_game(self):
+        quit_game = 0
+        while quit_game == 0:
+            winner = 1
+            for line in self.card1.card:
+                for item in line:
+                    if str(item).isnumeric():
+                        winner = 0
+            if winner == 1:
+                print('You won!')
+                break
+            winner = 1
+            for line in self.card2.card:
+                for item in line:
+                    if str(item).isnumeric():
+                        winner = 0
+            if winner == 1:
+                print('Computer won!')
+                break
+
+            rand_bag_num = random.randint(1, 90)
+            if rand_bag_num in self.bag_num:
+                continue
+            else:
+                self.bag_num.append(rand_bag_num)
+                print(f'The number is {rand_bag_num}')
+                card1.draw_card()
+                card2.draw_card()
+                error_input = 0
+                while True:
+                    user_answer = input('Would you like to cross the number?(y/n)').strip()
+                    for line_index, line in enumerate(self.card2.card):
+                        for item_index, item in enumerate(line):
+                            if rand_bag_num == item:
+                                self.card2.card[line_index][item_index] = '--'
+                    if user_answer == 'y':
+                        for line_index, line in enumerate(self.card1.card):
+                            for item_index, item in enumerate(line):
+                                if rand_bag_num == item:
+                                    self.card1.card[line_index][item_index] = '--'
+                                    sys.stdout.flush()
+                                    print('The choice is done correctly.\n', 27 * '*', '\n')
+                                    error_input = 1
+                        if error_input == 1:
+                            break
+                        print('Your choice is incorrect. You lost the game.')
+                        quit_game = 1
+                        break
+                    elif user_answer == 'n':
+                        for line_index, line in enumerate(self.card1.card):
+                            for item_index, item in enumerate(line):
+                                if rand_bag_num == item:
+                                    print('The choice is done incorrectly. You lost the game.')
+                                    quit_game = 1
+                                    error_input = 1
+                        if error_input == 1:
+                            break
+                        print('The choice is done correctly.\n', 27 * '*', '\n')
+                        break
+                    else:
+                        print('Input is incorrect.')
+                        continue
+
+
+
+
+
+
+card1 = LotoCard('-Player-')
+card2 = LotoCard('Computer')
+
+game = Game(card1, card2)
+game.start()
