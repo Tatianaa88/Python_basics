575f69f583bc039e7bfda6ba158fa3ff668cf9d3
Tatianaa88 <tatiana.v.ageeva@gmail.com>
2020-10-22 13:49:10 +0200

    Loto - done - final version.

--- Lesson_8/Lesson_8_Loto.py
+++ Lesson_8/Lesson_8_Loto.py
@@ -1,5 +1,5 @@
 import random
-import sys
+
 
 class LotoCard:
     def __init__(self, player):
@@ -10,6 +10,9 @@ class LotoCard:
         self.number_list = []
 
     def create_number(self):
+        '''
+        The method creates numbers for a card.
+        '''
         num_counter = 1
         while num_counter < 16:
             number = random.randint(1, 90)
@@ -30,43 +33,46 @@ class LotoCard:
         self.number_list.sort()
 
     def fillout_card(self):
-        for el in self.number_list:
-            i = 0
-            while i < 9:
-                upper_boarder = (i + 1) * 10
-                if i == 8:
+        '''
+        The method fills out a card with the numbers created by the 'create_number' method.
+        '''
+        self.create_number()
+        for self.el in self.number_list:
+            self.row = 0
+            while self.row < 9:
+                upper_boarder = (self.row + 1) * 10
+                if self.row == 8:
                     upper_boarder = 91
-                if i * 10 <= el < upper_boarder:
-                    counter = [[], [], []]
+                if self.row * 10 <= self.el < upper_boarder:
+                    self.counter = [[], [], []]
                     for j, line in enumerate(self.card):
                         for item in self.card[j]:
                             if item != '  ':
-                                counter[j].append(item)
-                    if len(counter[0]) >= len(counter[1]):
-                        if len(counter[1]) >= len(counter[2]):
-                            if self.card[2][i] == '  ':
-                                self.card[2][i] = el
-                            else:
-                                counter[2].append(0)
-                                i -= 1
-                                continue
+                                self.counter[j].append(item)
+                    if len(self.counter[0]) >= len(self.counter[1]):
+                        if len(self.counter[1]) >= len(self.counter[2]):
+                            self.row_add_element(2)
                         else:
-                            if self.card[1][i] == '  ':
-                                self.card[1][i] = el
-                            else:
-                                counter[1].append(0)
-                                i -= 1
-                                continue
+                            self.row_add_element(1)
                     else:
-                        if self.card[0][i] == '  ':
-                            self.card[0][i] = el
-                        else:
-                            counter[0].append(0)
-                            i -= 1
-                            continue
-                i += 1
+                        self.row_add_element(0)
+                self.row += 1
+
+    def row_add_element(self, card_row):
+        '''
+        The method adds an element into a card.
+        :param card_row: the number of the list(card's row) where the element is being added.
+        '''
+        if self.card[card_row][self.row] == '  ':
+            self.card[card_row][self.row] = self.el
+        else:
+            self.counter[card_row].append(0)
+            self.row -= 1
 
     def draw_card(self):
+        '''
+        The method draws a card.
+        '''
         print('-'* 9 + self.player + '-'* 10)
         for i in self.card:
             for index, j in enumerate(i):
@@ -83,85 +89,87 @@ class Game:
         self.card1 = card1
         self.card2 = card2
         self.bag_num = []
+        self.quit_game = 0
+        self.error_input = 0
 
     def start(self):
-        card1.create_number()
+        '''
+        The main method of the game.
+        '''
         card1.fillout_card()
-        card2.create_number()
         card2.fillout_card()
-        self.start_game()
-
-
-    def start_game(self):
-        quit_game = 0
-        while quit_game == 0:
-            winner = 1
-            for line in self.card1.card:
-                for item in line:
-                    if str(item).isnumeric():
-                        winner = 0
-            if winner == 1:
-                print('You won!')
-                break
-            winner = 1
-            for line in self.card2.card:
-                for item in line:
-                    if str(item).isnumeric():
-                        winner = 0
-            if winner == 1:
-                print('Computer won!')
-                break
-
-            rand_bag_num = random.randint(1, 90)
-            if rand_bag_num in self.bag_num:
+        while self.quit_game == 0:
+            self.rand_bag_num = random.randint(1, 90)
+            if self.rand_bag_num in self.bag_num:
                 continue
             else:
-                self.bag_num.append(rand_bag_num)
-                print(f'The number is {rand_bag_num}')
+                self.bag_num.append(self.rand_bag_num)
+                print(f'The number is {self.rand_bag_num}')
                 card1.draw_card()
                 card2.draw_card()
-                error_input = 0
-                while True:
+                self.error_input = 0
+                while self.error_input == 0:
                     user_answer = input('Would you like to cross the number?(y/n)').strip()
-                    for line_index, line in enumerate(self.card2.card):
-                        for item_index, item in enumerate(line):
-                            if rand_bag_num == item:
-                                self.card2.card[line_index][item_index] = '--'
+                    self.computer_answer_check()
+                    self.player_answer_check(user_answer)
+            self.check_winner(self.card1)
+            self.check_winner(self.card2)
+
+    def computer_answer_check(self):
+        '''
+        The method checks the computer's answer.
+        '''
+        for line_index, line in enumerate(self.card2.card):
+            for item_index, item in enumerate(line):
+                if self.rand_bag_num == item:
+                    self.card2.card[line_index][item_index] = '--'
+
+    def player_answer_check(self, user_answer):
+        '''
+        The method checks the user's answer.
+        :param user_answer: the exact answer that the user has given.
+        '''
+        for line_index, line in enumerate(self.card1.card):
+            for item_index, item in enumerate(line):
+                if self.rand_bag_num == item:
                     if user_answer == 'y':
-                        for line_index, line in enumerate(self.card1.card):
-                            for item_index, item in enumerate(line):
-                                if rand_bag_num == item:
-                                    self.card1.card[line_index][item_index] = '--'
-                                    sys.stdout.flush()
-                                    print('The choice is done correctly.\n', 27 * '*', '\n')
-                                    error_input = 1
-                        if error_input == 1:
-                            break
-                        print('Your choice is incorrect. You lost the game.')
-                        quit_game = 1
-                        break
-                    elif user_answer == 'n':
-                        for line_index, line in enumerate(self.card1.card):
-                            for item_index, item in enumerate(line):
-                                if rand_bag_num == item:
-                                    print('The choice is done incorrectly. You lost the game.')
-                                    quit_game = 1
-                                    error_input = 1
-                        if error_input == 1:
-                            break
+                        self.card1.card[line_index][item_index] = '--'
                         print('The choice is done correctly.\n', 27 * '*', '\n')
-                        break
+                        self.error_input = 1
+                        return
+                    elif user_answer == 'n':
+                        print('The choice is done incorrectly. You lost the game.')
+                        self.error_input = 1
+                        self.quit_game = 1
+                        return
                     else:
                         print('Input is incorrect.')
-                        continue
+        if user_answer == 'y':
+            print('The choice is done incorrectly. You lost the game.')
+            self.error_input = 1
+            self.quit_game = 1
+        elif user_answer == 'n':
+            print('The choice is done correctly.\n', 27 * '*', '\n')
+            self.error_input = 1
+        else:
+            print('Input is incorrect.')
 
-
-
-
+    def check_winner(self, participant):
+        '''
+        The method check whether a user or a computer has won.
+        :param participant: class instance.
+        '''
+        winner = 1
+        for line in participant.card:
+            for item in line:
+                if str(item).isnumeric():
+                    winner = 0
+        if winner == 1:
+            print(f'{participant.player} won!')
+            self.quit_game = 1
 
 
 card1 = LotoCard('-Player-')
 card2 = LotoCard('Computer')
-
 game = Game(card1, card2)
 game.start()
